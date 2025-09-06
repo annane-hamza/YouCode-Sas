b9a042fb5496f4f4e09fedb64dd022c966465fc4
h.annane <hamza.annane.office@gmail.com>
2025-09-06 10:58:24 +0100

    téléphone alphabétique

---
+++ Day02/09-challenge.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int     main()
+{
+    char number[100];
+
+    printf("Entrez un numéro de téléphone: ");
+    scanf("%s",&number);
+    
+    int count = 0;
+
+    while(number[count] != '\0')
+    {
+        if (number[count] == 'A' || number[count] == 'B' || number[count] == 'C')
+        {
+            printf("2");
+        }
+        else if (number[count] == 'D' || number[count] == 'E' || number[count] == 'F')
+        {
+            printf("3");
+        }
+        else if (number[count] == 'G' || number[count] == 'H' || number[count] == 'I')
+        {
+            printf("4");
+        }
+        else if (number[count] == 'J' || number[count] == 'K' || number[count] == 'L')
+        {
+            printf("5");
+        }
+
+        else if (number[count] == 'M' || number[count] == 'N' || number[count] == 'O')
+        {
+            printf("6");
+        }
+        else if (number[count] == 'P' || number[count] == 'R' || number[count] == 'S')
+        {
+            printf("7");
+        }
+
+        else if (number[count] == 'T' || number[count] == 'U' || number[count] == 'V')
+        {
+            printf("8");
+        }
+
+        else if (number[count] == 'W' || number[count] == 'X' || number[count] == 'Y')
+        {
+            printf("9");
+        }
+        else
+            printf("%c",number[count]);
+
+        count++;
+    }
+
+    return 0;
+}
