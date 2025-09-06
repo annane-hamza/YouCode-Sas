09919524b78afaaf800e17576a907905cf24e4ae
h.annane <hamza.annane.office@gmail.com>
2025-09-06 15:11:57 +0100

    Initialisation et Affichage

---
+++ Day03/01-challenge.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+    Écrivez un programme C qui initialise un tableau d'entiers avec des valeurs données et affiche ces valeurs. Par exemple,
+    vous pouvez initialiser un tableau avec les valeurs [1, 2, 3, 4, 5]
+    et afficher chaque valeur sur une nouvelle ligne.
+*/
+
+int main()
+{
+    int tab[] = {1,2,3,4,5};
+
+    for (int i = 0 ; i < 5 ; i++)
+    {
+        printf(" %d ",tab[i]);
+    }
+
+    printf("\n");
+}
+
